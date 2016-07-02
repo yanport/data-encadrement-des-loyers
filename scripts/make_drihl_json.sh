61fc336f9dc72932301172b851615ff6e9a6d395
François Massot <fmassot@yanport.com>
2016-07-03 00:45:16 +0200

    Update script to download drihl medians

--- scripts/make_drihl_json.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# USAGE: ./scripts/make_dhrip_json.sh
-
-mkdir data/tmp
-
-python scripts/parse_drihl_medians.py data/2015_drihl_medians.json
-
-rm -r data/tmp
--- scripts/parse_drihl_medians.py
+++ scripts/parse_drihl_medians.py
@@ -1,16 +1,34 @@
 # -*- coding: utf-8 -*-
 
-import io
 import sys
-import json
+import csv
+import io
 import requests
 import xml.etree.ElementTree as ET
+import itertools
 
-BASE_URL = 'https://www.referidf.com/kml'
+from enum import Enum
 
 
-def parse_kml(response):
-    kml = ET.fromstring(response.content.decode('utf-8').replace(' xmlns="http://earth.google.com/kml/2.1"', ''))
+class Period(Enum):
+    before_1946 = 'inf1946'
+    between_1946_1970 = '1946-1970'
+    between_1971_1990 = '1971-1990'
+    after_1990 = 'sup1990'
+
+
+def build_drihl_url(year, room_count, period, furnished):
+    assert room_count in range(1, 5)
+    assert year in [2015, 2016]
+
+    date = '2016-08-01' if year == 2016 else '2015-08-01'
+    furnished = 'meuble' if furnished else 'non-meuble'
+
+    return 'http://www.referenceloyer.drihl.ile-de-france.developpement-durable.gouv.fr/kml/%s/drihl_medianes_%s_%s_%s.kml?t=20150327' % (date, room_count, period.value, furnished)
+
+
+def parse_kml(response, year):
+    kml = ET.fromstring(response.text.replace(' xmlns="http://earth.google.com/kml/2.1"', ''))
 
     document = kml.getchildren()[0]
 
@@ -22,7 +40,7 @@ def parse_kml(response):
             attr_name = data.attrib['name']
 
             if attr_name in ['ref', 'refmaj', 'refmin']:
-                element[attr_name] = round(float(data.findtext('value').replace(',', '.')), 1)
+                element['%s_%s' %(year, attr_name)] = round(float(data.findtext('value').replace(',', '.')), 1)
             else:
                 element[attr_name] = data.findtext('value')
 
@@ -33,20 +51,26 @@ def parse_kml(response):
 
 def fetch_and_parse_kml():
     output = []
-
-    total = 4 * 4 * 2
+    total = 4 * 4 * 2 * 2
     print("#{0} pages to download".format(total))
 
     count = 0
-    for room_count in range(1, 5):
-        for period in ["inf1946", "1946-1970", "1971-1990", "sup1990"]:
-            for is_furshished in ["meuble", "non-meuble"]:
-                url = "%s/drihl_medianes_%s_%s_%s.kml" % (BASE_URL, room_count, period, is_furshished)
+    for room_count, period, furnished in itertools.product(range(1, 5), Period, [False, True]):
+        results_by_year = []
+        for year in [2015, 2016]:
+            count += 1
+            url = build_drihl_url(year, room_count, period, furnished)
 
-                count += 1
-                print ("download url {0} - {1}/{2}".format(url, count, total))
+            print ("download url {0} - {1}/{2}".format(url, count, total))
 
-                output += parse_kml(requests.get(url, params={"t": "20150327"}))
+            response = requests.get(url)
+
+            results_by_year.append(parse_kml(response, year))
+
+        for row_2015, row_2016 in zip(results_by_year[0], results_by_year[1]):
+            row_2015.update(row_2016)
+
+        output += results_by_year[0]
 
     return output
 
@@ -54,5 +78,9 @@ def fetch_and_parse_kml():
 if __name__ == '__main__':
     output_file = sys.argv[1]
 
-    with io.open(output_file, 'w', encoding='utf8') as f:
-        json.dump(fetch_and_parse_kml(), f, indent=4, ensure_ascii=False, sort_keys=True)
+    with io.open(output_file, 'w', encoding='utf8') as csvfile:
+        data = fetch_and_parse_kml()
+
+        writer = csv.DictWriter(csvfile, fieldnames=['idZone', 'nameZone', 'idQuartier', 'piece', 'epoque', 'type', 'ref_2015', 'ref_2016', 'refmin_2015', 'refmin_2016', 'refmaj_2015', 'refmaj_2016'])
+        writer.writeheader()
+        writer.writerows(data)
